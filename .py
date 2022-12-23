20edb89d1ab161478c1878b8c7230392c7f893ca
KOMMARAJULAKSHMISAISASANK <120789061+KOMMARAJULAKSHMISAISASANK@users.noreply.github.com>
2022-12-23 18:21:57 +0530

    code

--- .py
+++ .py
@@ -7,3 +7,49 @@ import librosa
 
 def main():
   selected_box = st.sidebar.selectbox('Choose an option...',('Emotion Recognition','view source code'))
+  if selected_box == 'emotion recognition':
+    st.sidebar.success('to try yourself by adding a audio file:')
+    application()
+  if selected_box == 'view source code':
+    st.code(get_file_content_as_string("code.py"))
+    
+@st.cache(show_spinner=False)
+def get_file_content_as_string(path):
+  url = ''+path
+  response = urllib.request.urlopen(url)
+  return response.read().decode("utf-8")
+
+@st.cache(show_spinner=False)
+def load_model():
+  model=tf.keras.models.load_model('mymodel.h5')
+  return model
+def application():
+  models_load_state=st.text('\n loading models..')
+  model=load_model()
+  model_load_state.text('\n models loading..completed')
+  
+  
+  file_to_be_uploaded = st.file_uploader("choose an audio file...", type="wav")
+  
+  if file_to_be_uploaded:
+    st.audio(file_to_be_uploaded, format='audio/wav')
+    st.success('emotion of the audio is '+predict(model,file_to_be_uploaded))
+    
+def extract_mfcc(wav_file_name):
+  y,sr = librosa.load(wav_file_name)
+  mfccs = np.mean(librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40).T,axis=0)
+  
+  return mfccs
+
+def predict(model_filepath):
+  emotions={1:'neutral', 2: 'calm', 3:'happy', 4: 'sad', 5: 'angry', 6: 'fearfull', 7:'disgust', 8:'surprised'}
+  test_point=extract_mfcc(wav_filepath)
+  test_point=np.reshape(test_point,newshape=(1,40,1))
+  predictions=model.predict(test_point)
+  print(emotions[np.argmax(predictions[0])+1])
+  
+  return emotions[np.argmax(predictions[0])+1]
+
+if __name__ == "__main__":
+  main()
+                  
